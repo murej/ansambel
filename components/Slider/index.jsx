ed112e1543d43b13719c5d8e543771351c54b707
murej <jmartinec@mu-m-jmartinec.lan>
2016-07-24 05:36:03 +0200

    Content update

--- components/Slider/index.jsx
+++ components/Slider/index.jsx
@@ -8,55 +8,177 @@ import Slide from '../Slide'
 
 const slides = [
   <Slide
-    key="0"
-    type={'normal'}
-    imageUrl={prefixLink('/assets/images/fotopub4.jpg')}
-    caption={'prvi'}
-    ratios={{
-      horizontal: [1,3,8],  // razmerje širine grida
-      vertical: [20,20,60]  // procent višine screena
-    }}
-  />,
-  <Slide
-    key="1"
-    type={'bleeding'}
-    imageUrl={prefixLink('/assets/images/fullscreen.jpg')}
-    caption={'drugi'}
-  />,
-  <Slide
-    key="2"
-    type={'normal'}
-    imageUrl={prefixLink('/assets/images/koordinate_zvoka_2.jpg')}
-    caption={'tretji'}
-    ratios={{
-      horizontal: [2,6,2],
-      vertical: [50,6,6]
-    }}
-  />,
-  <Slide
-    key="3"
-    type={'bleeding'}
-    imageUrl={prefixLink('/assets/images/_G4A8913.jpg')}
-    caption={'četrti'}
-  />,
-  <Slide
     key="4"
     type={'normal'}
-    imageUrl={prefixLink('/assets/images/4.jpg')}
-    caption={'peti'}
+    imageUrl={prefixLink('/assets/images/ansambel5.jpg')}
+    caption={'Hud naslov, installation shot'}
     ratios={{
-      horizontal: [2,6,2],
-      vertical: [2,6,6]
+      horizontal: [1,7,4],  // razmerje širine grida
+      vertical: [10,80,10]  // procent višine screena
+    }}
+  />,
+  <Slide
+    key="15"
+    type={'normal'}
+    imageUrl={prefixLink('/assets/images/ansambel16.jpg')}
+    caption={'Can squeeze jewelry inside layers of foam'}
+    ratios={{
+      horizontal: [6,6,0],
+      vertical: [10,80,10]
+    }}
+  />,
+  <Slide
+    key="12"
+    type={'normal'}
+    imageUrl={prefixLink('/assets/images/ansambel13.jpg')}
+    caption={'Bathers'}
+    ratios={{
+      horizontal: [3,6,3],
+      vertical: [5,90,5]
+    }}
+  />,
+  <Slide
+    key="10"
+    type={'normal'}
+    imageUrl={prefixLink('/assets/images/ansambel11.jpg')}
+    caption={'A catalog for Fotopub, a photography festival in Novo mesto (with custom flags)'}
+    ratios={{
+      horizontal: [6,6,0],
+      vertical: [40,50,10]
     }}
   />,
   <Slide
     key="5"
     type={'normal'}
-    imageUrl={prefixLink('/assets/images/Dan_documentation.jpg')}
-    caption={'šesti'}
+    imageUrl={prefixLink('/assets/images/ansambel6.jpg')}
+    caption={'Satellite dish, photo of Saturunus GX 700'}
     ratios={{
-      horizontal: [2,6,2],
-      vertical: [2,6,6]
+      horizontal: [4,6,2],
+      vertical: [20,40,40]
+    }}
+  />,
+  <Slide
+    key="8"
+    type={'normal'}
+    imageUrl={prefixLink('/assets/images/ansambel9.jpg')}
+    caption={'“Electricity is just like WHOA!” website'}
+    ratios={{
+      horizontal: [1,11,6],
+      vertical: [25,70,5]
+    }}
+  />,
+  <Slide
+    key="0"
+    type={'bleeding'}
+    imageUrl={prefixLink('/assets/images/ansambel1.jpg')}
+    caption={'Trust me it’s Art, an online gallery of passwords'}
+  />,
+  <Slide
+    key="6"
+    type={'normal'}
+    imageUrl={prefixLink('/assets/images/ansambel7.jpg')}
+    caption={'Locks and Keys'}
+    ratios={{
+      horizontal: [5,6,1],
+      vertical: [5,90,5]
+    }}
+  />,
+  <Slide
+    key="9"
+    type={'normal'}
+    imageUrl={prefixLink('/assets/images/ansambel10.jpg')}
+    caption={'A standby lamp that emits light in the dark'}
+    ratios={{
+      horizontal: [1,7,4],
+      vertical: [10,80,10]
+    }}
+  />,
+  <Slide
+    key="2"
+    type={'bleeding'}
+    imageUrl={prefixLink('/assets/images/ansambel3.jpg')}
+    caption={'Branko Oblak, football player'}
+  />,
+  <Slide
+    key="22"
+    type={'normal'}
+    imageUrl={prefixLink('/assets/images/ansambel23.jpg')}
+    caption={'Zaza, an illustration for an article in Tribuna magazine'}
+    ratios={{
+      horizontal: [1,6,5],
+      vertical: [5,88,7]
+    }}
+  />,
+  <Slide
+    key="11"
+    type={'normal'}
+    imageUrl={prefixLink('/assets/images/ansambel12.jpg')}
+    caption={'Dreaming of vacation while pouring concrete, (creative direction for a series of musical events in Ljubjana, organized by Žiga Murko)'}
+    ratios={{
+      horizontal: [5,6,1],
+      vertical: [10,80,10]
+    }}
+  />,
+  <Slide
+    key="21"
+    type={'normal'}
+    imageUrl={prefixLink('/assets/images/ansambel22.jpg')}
+    caption={'Wires, photography'}
+    ratios={{
+      horizontal: [2,6,4],
+      vertical: [4,78,18]
+    }}
+  />,
+  <Slide
+    key="3"
+    type={'bleeding'}
+    imageUrl={prefixLink('/assets/images/ansambel4.gif')}
+    caption={'Live Screen, a mirror which only reflects when activated'}
+  />,
+  <Slide
+    key="20"
+    type={'normal'}
+    imageUrl={prefixLink('/assets/images/ansambel21.jpg')}
+    caption={'TDC Tokyo, an identity for an exhibiton in Tokyo'}
+    ratios={{
+      horizontal: [4,7,1],
+      vertical: [10,85,5]
+    }}
+  />,
+  <Slide
+    key="14"
+    type={'normal'}
+    imageUrl={prefixLink('/assets/images/ansambel15.jpg')}
+    caption={'Dragons in a Baththub, an illustrated fairytale, special issue for Tribuna magazine'}
+    ratios={{
+      horizontal: [1,9,6],
+      vertical: [0,100,0]
+    }}
+  />,
+  <Slide
+    key="13"
+    type={'normal'}
+    imageUrl={prefixLink('/assets/images/ansambel14.jpg')}
+    caption={'A led-based drawing keyboard'}
+    ratios={{
+      horizontal: [3,7,2],
+      vertical: [0,70,30]
+    }}
+  />,
+  <Slide
+    key="1"
+    type={'bleeding'}
+    imageUrl={prefixLink('/assets/images/ansambel2.jpg')}
+    caption={'Čačke, a wallpaper pattern'}
+  />,
+  <Slide
+    key="19"
+    type={'normal'}
+    imageUrl={prefixLink('/assets/images/ansambel20.jpg')}
+    caption={'Room service'}
+    ratios={{
+      horizontal: [4,6,2],
+      vertical: [20,40,40]
     }}
   />,
 ]
@@ -64,7 +186,7 @@ const slides = [
 export default class SliderComponent extends React.Component {
   constructor () {
     super()
-    const randomStart = 0//_.random(0, slides.length - 1)
+    const randomStart = _.random(0, slides.length - 1)
     for(let i=0; i<randomStart; i++) {
       slides.push(slides.shift());
     }
@@ -107,7 +229,6 @@ export default class SliderComponent extends React.Component {
     })
   }
   render () {
-    console.log(this.state);
     return (
       <div className="Slider">
         <div
